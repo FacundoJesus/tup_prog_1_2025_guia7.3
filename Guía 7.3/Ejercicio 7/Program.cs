6a51b65f9cd2a2a325d28ef73bb93bae53dba891
FacuJesus <facundojesus10@hotmail.com>
2025-05-18 10:41:58 -0300

    Matriz (Paras anidados)

---
+++ Guía 7.3/Ejercicio 7/Program.cs
@@ -0,0 +1,54 @@
+﻿namespace Ejercicio_7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int valor, acumulador, nGrupoMayorPromedio, nGrupoMenorPromedio;
+            double promedio, mayorPromedio, menorPromedio;
+
+            //Iterar 4 grupos
+            mayorPromedio = 0;
+            menorPromedio=0;
+            nGrupoMayorPromedio = 0;
+            nGrupoMenorPromedio = 0;
+            for (int i = 1; i <= 4; i++)
+            {
+                Console.WriteLine($"Grupo: Nº"+i);
+
+                //Iterar seis valores
+                acumulador = 0;
+                for (int j = 1; j <= 6; j++)
+                {
+                    Console.WriteLine($"Ingrese el {j}º valor obtenido:");
+                    valor = Convert.ToInt32(Console.ReadLine());
+                    acumulador += valor;
+                }
+
+                promedio = (acumulador * 1.0) / 6;
+                Console.WriteLine($"Promedio: {promedio:f2}");
+
+                if (i == 1)
+                {
+                    mayorPromedio = promedio;
+                    nGrupoMayorPromedio = i;
+                    menorPromedio = promedio;
+                    nGrupoMenorPromedio = i;
+                }
+
+                if (promedio > mayorPromedio) {
+                    mayorPromedio = promedio;
+                    nGrupoMayorPromedio = i;
+                }
+                if (promedio < menorPromedio)
+                {
+                    menorPromedio = promedio;
+                    nGrupoMenorPromedio = i;
+                }
+            }
+            Console.WriteLine($"Nº Grupo con Mayor Promedio: {nGrupoMayorPromedio}, Mayor Promedio: {mayorPromedio:f2}");
+            Console.WriteLine($"Nº Grupo con Menor Promedio: {nGrupoMenorPromedio}, Menor Promedio: {menorPromedio:f2}");
+        }
+    }
+}
