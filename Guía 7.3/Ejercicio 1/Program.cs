5537979a462f7e19ba8da4280b05e754546c24fb
FacuJesus <facundojesus10@hotmail.com>
2025-05-18 00:58:42 -0300

    Promedio de 20 números

---
+++ Guía 7.3/Ejercicio 1/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Ejercicio_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, num;
+            double acumulador,promedio;
+
+
+            acumulador = 0;
+            for (i = 1; i <= 3; i ++) {
+                Console.WriteLine("Ingrese un número:");
+                num = Convert.ToInt32(Console.ReadLine());
+                acumulador += num;
+            }
+            
+            promedio = (acumulador) / (i-1);
+            Console.WriteLine($"El promedio de los 20 números es: {promedio:f2}");
+        }
+    }
+}
